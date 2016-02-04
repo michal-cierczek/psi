6883d615bc7769fd81c3b7fc5049a8ccd550f32a
Mariusz <192573@student.pwr.wroc.pl>
2016-02-04 02:14:38 +0100

    Widget do przedmiotu

--- views/przedmiot/update.php
+++ views/przedmiot/update.php
@@ -1,15 +1,39 @@
 <?php
 
 use yii\helpers\Html;
+use kartik\sidenav\SideNav;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Przedmiot */
 
-$this->title = 'Update Przedmiot: ' . ' ' . $model->id;
+$this->title = 'Edycja karty przedmiotu. Krok: ' . ' ' . $step;
 $this->params['breadcrumbs'][] = ['label' => 'Przedmiots', 'url' => ['index']];
 //$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'kierunekStudiow_id' => $model->kierunekStudiow_id, 'user_id' => $model->user_id]];
 $this->params['breadcrumbs'][] = 'Update';
+$id = $model->id;
 ?>
+<div class="col-sm-3">
+<?= SideNav::widget([
+		'type' => SideNav::TYPE_DEFAULT,
+		'encodeLabels' => false,
+		'heading' => false,
+		'items' => [
+				['label' => 'Informacje podstawowe', 'url' => Url::to(['update', 'id' => $id, 'step'=>'1'])],
+				['label' => 'Kursy', 'url' => Url::to(['update', 'id' => $id, 'step'=>'2'])],
+				['label' => 'Wymagania', 'url' => Url::to(['update', 'id' => $id, 'step'=>'3'])],
+				['label' => 'Cele przedmiotu', 'url' => Url::to(['update', 'id' => $id, 'step'=>'4'])],
+				['label' => 'PEK', 'url' => Url::to(['update', 'id' => $id, 'step'=>5])],
+				['label' => 'Treści programowe', 'url' => Url::to(['update', 'id' => $id, 'step'=>'6'])],
+				['label' => 'Narzędzia dydaktyczne', 'url' => Url::to(['update', 'id' => $id, 'step'=>'7'])],
+				['label' => 'Ocena osiągnięcia PEK', 'url' => Url::to(['update', 'id' => $id, 'step'=>'8'])],
+				['label' => 'Literatura', 'url' => Url::to(['update', 'id' => $id, 'step'=>'9'])],
+				['label' => 'Opiekun', 'url' => Url::to(['update', 'id' => $id, 'step'=>'10'])],
+				['label' => 'Macierz powiązania', 'url' => Url::to(['update', 'id' => $id, 'step'=>'11'])],
+		],
+]); ?>
+</div>
+<div class="col-sm-9">
 <div class="przedmiot-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -17,5 +41,5 @@ $this->params['breadcrumbs'][] = 'Update';
     <?= $this->render('kroki/' . $step, [
         'model' => $model,
     ]) ?>
-
+</div>
 </div>
