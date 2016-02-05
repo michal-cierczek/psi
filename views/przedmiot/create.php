b552004f488b0b8a456d4deccd57054921a3b2fe
Michał Cierczek <192578@student.pwr.wroc.pl>
2016-02-05 22:15:19 +0100

    sg

--- views/przedmiot/create.php
+++ views/przedmiot/create.php
@@ -1,21 +1,39 @@
 <?php
 
 use yii\helpers\Html;
-
+use kartik\sidenav\SideNav;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Przedmiot */
 
-$this->title = 'Create Przedmiot';
+$this->title = 'Edycja karty przedmiotu. Krok: ' . ' ' . $step;
 $this->params['breadcrumbs'][] = ['label' => 'Przedmiots', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'kierunekStudiow_id' => $model->kierunekStudiow_id, 'user_id' => $model->user_id]];
+$this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="przedmiot-create">
+<div class="col-sm-3">
+<?= SideNav::widget([
+		'type' => SideNav::TYPE_DEFAULT,
+		'encodeLabels' => false,
+		'heading' => false,
+		'items' => [
+				['label' => 'Informacje podstawowe', 'url' => Url::to(['update', 'id' => $id, 'step'=>'1'])],
+				
+				['label' => 'Opiekun', 'url' => Url::to(['update', 'id' => $id, 'step'=>'10'])],
+				
+		],
+]); ?>
+</div>
+<div class="col-sm-9">
+<div class="przedmiot-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render($step, [
+    <?= $this->render('kroki/' . $step, [
         'model' => $model,
+    	'forModal' => $forModal,
+    	'id' => $id
     ]) ?>
-
+</div>
 </div>
