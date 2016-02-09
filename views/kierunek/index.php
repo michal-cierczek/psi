26bb63ac587670044b5eb707f8d65b32c2a03133
Marcin Klimiuk <192550@student.pwr.edu.pl>
2016-02-09 11:07:30 +0100

    hardcoded rodzaj przedmiotu na karcie

--- views/kierunek/index.php
+++ views/kierunek/index.php
@@ -27,6 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
         		'label' => 'Nazwa'
         	],
             'skrot',
+        	'specjalnosc',
         	'forma',
         	'stopien',
         	'cykl',
